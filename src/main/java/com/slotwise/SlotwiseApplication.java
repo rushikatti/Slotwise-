a63179a825fd498200e100f1a08fded25bf12d2c
Rushi Katti <rushi111011@gmail.com>
2026-09-02 20:46:51 +0530

    Day 1: project skeleton with Postgres connection

---
+++ src/main/java/com/slotwise/SlotwiseApplication.java
@@ -0,0 +1,13 @@
+package com.slotwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slotwise/SlotwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slotwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
